35a51dcf85b868e104360fdbb77cdafc3e148f9c
Michael145st <119664643+Michael145st@users.noreply.github.com>
2023-10-01 19:43:55 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -318,17 +318,20 @@ function loadContent() {
 				) {
 					const imageUrl = image.fields.file.url
 
+					// Извлекаем текст описания из объекта "description"
+					const descriptionText = project.fields.description.text
+
 					// Создаем HTML для проекта (подставьте свои поля из Contentful)
 					projectDiv.innerHTML = `
-                        <div class="background">
-                            <img src="${imageUrl}" alt="${project.fields.title}">
-                        </div>
-                        <div class="onhover">
-                            <h1 class="title">${project.fields.title}</h1>
-                            <p class="description">${project.fields.description}</p>
-                            <button><a href="${project.fields.link}" target="_blank">Открыть</a></button>
-                        </div>
-                    `
+            <div class="background">
+                <img src="${imageUrl}" alt="${project.fields.title}">
+            </div>
+            <div class="onhover">
+                <h1 class="title">${project.fields.title}</h1>
+                <p class="description">${descriptionText}</p>
+                <button><a href="${project.fields.link}" target="_blank">Открыть</a></button>
+            </div>
+        `
 
 					// Добавляем проект в контейнер
 					container.appendChild(projectDiv)
